4b9b9c0f756df5ed75e922fd2245bbce5bea0737
Lisakarolina <w.hello@web.de>
2026-06-24 20:50:57 +0200

    Add lab partner to student model

--- api/models.py
+++ api/models.py
@@ -86,6 +86,14 @@ class Student(models.Model):
         related_name="students",
     )
 
+    lab_partner = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="lab_partner_of",
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
