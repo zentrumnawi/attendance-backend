6f78577af83a8723440177b6b4692b3f658f2166
Lisakarolina <w.hello@web.de>
2026-06-29 21:48:08 +0200

    Clear field of old group

--- api/utils/teaching_assistant.py
+++ api/utils/teaching_assistant.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from api.models import Group
 
 
 @transaction.atomic
@@ -14,3 +15,9 @@ def sync_teaching_assistant_for_user_profile(profile, old_group_id, new_group_id
     if group.teaching_assistant_id != profile.user_id:
         group.teaching_assistant = profile.user
         group.save(update_fields=["teaching_assistant"])
+
+    if old_group_id:
+        old_group = Group.objects.get(id=old_group_id)
+        if old_group.teaching_assistant_id == profile.user_id:
+            old_group.teaching_assistant = None
+            old_group.save(update_fields=["teaching_assistant"])
