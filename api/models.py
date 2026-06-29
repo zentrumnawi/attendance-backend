e9b731e23325110cfe2412a552d532894449cd29
Lisakarolina <w.hello@web.de>
2026-06-29 11:44:38 +0200

    Add field to group model

--- api/models.py
+++ api/models.py
@@ -24,6 +24,7 @@ class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
+    teaching_assistant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
