517b1a0391b376d87c7c6ab5d0adb0063805affb
Lisakarolina <w.hello@web.de>
2026-08-01 22:48:41 +0200

    Only count main authored protocols

--- api/models.py
+++ api/models.py
@@ -381,7 +381,7 @@ class FinalResult(models.Model):
 
     @property
     def papers_completed(self) -> int:
-        return self.student.paper_submissions.filter(submitted=True).count()
+        return self.student.paper_submissions.filter(submitted=True, main_author=True).count()
 
     @property
     def exercises_completed(self) -> int:
